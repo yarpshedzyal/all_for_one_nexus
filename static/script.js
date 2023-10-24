f8fcb3ca1ba1bf97d397a1c3addf7d5a7109399f
yar.pshe <yar.pshe@gmail.com>
2023-10-24 14:22:54 +0300

    pagination(cant chose number og items per page)

--- static/script.js
+++ static/script.js
@@ -214,4 +214,128 @@ document.addEventListener('DOMContentLoaded', function () {
     customizeCancelButton.addEventListener('click', hideCustomizeDialog);
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const prevPageButton = document.getElementById('prev-page');
+    const nextPageButton = document.getElementById('next-page');
+    const currentPageSpan = document.getElementById('current-page');
+    const itemsPerPageSelect = document.getElementById('items-per-page');
+    let itemsPerPage = parseInt(itemsPerPageSelect.value); // Initialize with default value
+    
+    // Define the initial values for pagination
+    let currentPage = 1;           // Initialize the current page
+    // let itemsPerPage = 50;         // Initialize the items per page (default: 50)
+    
 
+    itemsPerPageSelect.addEventListener('change', function () {
+        itemsPerPage = parseInt(this.value);
+        currentPage = 1; // Reset to the first page when changing items per page
+        fetchData(currentPage, itemsPerPage); // Fetch data with the new items per page value
+    });
+
+    // Function to update the table based on the current page and items per page
+    function updateTable() {
+        // Calculate the start and end indices for the displayed items
+        const startIndex = (currentPage - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+
+        // Get all table rows except for the header
+        const tableRows = document.querySelectorAll('tr:not(.header)');
+        
+        // Loop through table rows and hide/show based on the current page and items per page
+        tableRows.forEach((row, index) => {
+            if (index >= startIndex && index < endIndex) {
+                row.style.display = 'table-row'; // Show rows within the current range
+            } else {
+                row.style.display = 'none';      // Hide rows outside the current range
+            }
+        });
+        
+        // Update the current page number display
+        currentPageSpan.textContent = currentPage;
+    }
+
+    // Event listener for the "Previous" button
+    prevPageButton.addEventListener('click', function () {
+        if (currentPage > 1) {
+            currentPage--;
+            updateTable();
+        }
+    });
+
+    // Event listener for the "Next" button
+    nextPageButton.addEventListener('click', function () {
+        // Calculate the total number of pages based on the number of items
+        const totalItems = document.querySelectorAll('tr:not(.header)').length;
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+        
+        if (currentPage < totalPages) {
+            currentPage++;
+            updateTable();
+        }
+    });
+
+    // // Event listener for the items-per-page dropdown
+    // itemsPerPageSelect.addEventListener('change', function () {
+    //     itemsPerPage = parseInt(this.value);
+    //     currentPage = 1; // Reset to the first page when changing items per page
+    //     updateTable();
+    // });
+
+    // // Initialize the page with the default settings
+    // updateTable();
+});
+
+// // Fetch data function
+// function fetchData(page, perPage) {
+//     // Make an AJAX request to the server with the page and perPage parameters
+//     fetch(`/fetch_data?page=${page}&per_page=${perPage}`)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             // Update the table with the new data
+//             updateTable(data.items);
+
+//             // Update the current page number and total number of pages
+//             updatePagination(data.currentPage, data.totalPages);
+//         })
+// }
+
+// // Function to update the table with new data
+// function updateTable(items) {
+//     const table = document.getElementById('table-body');
+//     // const tbody = table.getElementById('table-body');
+
+//     // Clear existing table rows
+//     table.innerHTML = '';
+
+//     // Loop through the items and add rows to the table
+//     // Populate the table rows with data
+//     items.forEach(item => {
+//         const row = document.createElement('tr');
+//         for (const key in item) {
+//             if (item.hasOwnProperty(key)) {
+//                 const cell = document.createElement('td');
+//                 cell.textContent = item[key];
+//                 row.appendChild(cell);
+//             }
+//         }
+//         table.appendChild(row);
+//     });
+// }
+
+
+// // Function to update the current page number and total number of pages
+// function updatePagination(currentPage, totalPages) {
+//     const currentPageSpan = document.getElementById('current-page');
+//     const totalPagesSpan = document.getElementById('total-pages');
+
+//     // Update the current page number
+//     currentPageSpan.textContent = currentPage;
+
+//     // Update the total number of pages
+//     totalPagesSpan.textContent = totalPages;
+// }
