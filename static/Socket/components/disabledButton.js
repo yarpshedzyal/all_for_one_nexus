f73f90766ca284e624dc527444378be89b3b262a
Brudrok21 <Tracho2013@gmail.com>
2023-12-04 23:51:03 +0200

    done

--- static/Socket/components/disabledButton.js
+++ static/Socket/components/disabledButton.js
@@ -4,19 +4,19 @@ function disabledButton(socket) {
   let PADPB = document.querySelector("#parse-all-delivery-prices-button");
   let PAPB = document.querySelector("#parse-all-prices-button")
   socket.on("delivery_all_parse_started", async (res) => {
-    console.log(res);
+    // console.log(res);
     PADPB.disabled = res.disabled;
   });
   socket.on("parse_all_started", async (res) => {
-    console.log(res);
+    // console.log(res);
     PAPB.disabled = res.disabled;
   });
   socket.on("selected_parse_started", async (res) => { 
-    console.log(res);
+    // console.log(res);
     ParseSelectedPrices.disabled = res.disabled;
   });
   socket.on("delivery_selected_parse_started", async (res) => {
-    console.log(res);
+    // console.log(res);
     ParseSelectedDeliveryPrices.disabled = res.disabled;
   });
 }
